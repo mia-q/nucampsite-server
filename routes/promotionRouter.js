72434c9529348fd32bdcd4bec04eeda0ee061d1d
Mia Quintana <mia.quintana@live.com>
2024-02-17 21:02:12 -0500

    week 3

--- routes/promotionRouter.js
+++ routes/promotionRouter.js
@@ -13,7 +13,7 @@ promotionRouter.route('/')
     })
     .catch(err => next(err));
 })
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.create(req.body)
     .then(promotion => {
         console.log('partner created', promotion);
@@ -27,7 +27,7 @@ promotionRouter.route('/')
     res.statusCode = 403;
     res.end('PUT opertain not supported on /promotions')
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotion.deleteMany()
     .then(response => {
         res.statusCode = 200;
@@ -37,9 +37,9 @@ promotionRouter.route('/')
     .catch(err => next(err));
 });
 
-promotionRouter.route(':promotionId')
+promotionRouter.route(':promotionsId')
 .get((req, res, next) => {
-    Promotion.findById(req.params.promotionId)
+    Promotion.findById(req.params.promotionsId)
     .then(promotion => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
@@ -49,10 +49,10 @@ promotionRouter.route(':promotionId')
 })
 .post(authenticate.verifyUser, (req, res)=> {
     res.statusCode = 403;
-    res.end(`POST operation is not supported on /promoitons/${req.params.promotionId}`);
+    res.end(`POST operation is not supported on /promoitons/${req.params.promotionsId}`);
 })
-.put((req, res, next)=> {
-    Promotion.findByIdAndUpdate((req.params.promotionId), {
+.put(authenticate,verifyUser, authenticate.verifyAdmin, (req, res, next)=> {
+    Promotion.findByIdAndUpdate((req.params.promotionsId), {
         $set: req.body
     }, { new: true })
     .then(promotion => {
@@ -62,8 +62,8 @@ promotionRouter.route(':promotionId')
     })
     .catch(err => next(err))
 })
-.delete(authenticate.verifyUser, (req, res) => {
-    Promotion.findByIdAndDelete(req.params.promotionId)
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res) => {
+    Promotion.findByIdAndDelete(req.params.promotionsId)
     .then(response => {
         res.statusCode = 200;
         res.setHeader('Content-Type', 'application/json');
--- routes/users.js
+++ routes/users.js
@@ -15,28 +15,53 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.send('respond with a resource');
+    if (req.user.admin) {
+        User.find()
+        .then(users=> {
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json(users);
+    })
+    } else {
+        const err = new Error('You are not authorized to view this page');
+        return err;
+    }
 });
 
-router.post('/signup', (req, res, next) => {
-  User.register(
-    new User({username: req.body.username}),
-    req.body.password,
-    err => {
-        if (err) {
-            res.statusCode = 500;
-            res.setHeader('Content-Type', 'application/json');
-            res.json({err: err});
-        } else {
-            passport.authenticate('local')(req, res, () => {
-                res.statusCode = 200;
+router.post('/signup', (req, res) => {
+    User.register(
+        new User({username: req.body.username}),
+        req.body.password,
+        (err, user) => {
+            if (err) {
+                res.statusCode = 500;
                 res.setHeader('Content-Type', 'application/json');
-                res.json({success: true, status: 'Registration Successful!'});
-            });
+                res.json({err: err});
+            } else {
+                if (req.body.firstname) {
+                    user.firstname = req.body.firstname;
+                }
+                if (req.body.lastname) {
+                    user.lastname = req.body.lastname;
+                }
+                user.save(err => {
+                    if (err) {
+                        res.statusCode = 500;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({err: err});
+                        return;
+                    }
+                    passport.authenticate('local')(req, res, () => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Type', 'application/json');
+                        res.json({success: true, status: 'Registration Successful!'});
+                    });
+                });
+            }
         }
-    }
-);
+    );
 });
+
 
 router.post('/login', passport.authenticate('local'), (req, res) => {
   const token = authenticate.getToken({_id: req.user._id});
@@ -56,5 +81,7 @@ router.get('/logout', (req, res, next) => {
         return next(err);
     }
 });
+
+
 
 module.exports = router;
